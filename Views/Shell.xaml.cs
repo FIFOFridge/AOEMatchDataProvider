b42ef40fb0e51553bc6d78b4f2febe856e17cc91
FIFOFridge <FIFOFridge>
2021-02-09 12:56:49 +0100

    Reworked unwanted windows input processing - removed low level window hooks - added auto window hide by checking distance between mouse cursor and window position

--- Views/Shell.xaml.cs
+++ Views/Shell.xaml.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -31,27 +33,34 @@ namespace AOEMatchDataProvider.Views
 
             set
             {
-                //make sure hook is set
                 if (value == true)
-                {
-                    if (!IsWindowProcHooked)
-                        HookWindowProc();
-                }
+                    EnableIgnoreInput();
+                else
+                    DisableIgnoreInput();
 
                 ignoreInput = value;
             }
         }
 
-        bool IsWindowProcHooked
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
         {
-            get
-            {
-                return hwndSourceHook != null;
-            }
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new Point(w32Mouse.X, w32Mouse.Y);
         }
 
-        HwndSource source;
-        HwndSourceHook hwndSourceHook;
+        Timer refreshMousePositionTimer;
 
         public Shell()
         {
@@ -64,36 +73,71 @@ namespace AOEMatchDataProvider.Views
 
         Window IShell.Shell => this;
 
-        void HookWindowProc()
+        void EnableIgnoreInput()
         {
-            if (!IsWindowProcHooked)
+            if(refreshMousePositionTimer != null)
             {
-                source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
-                hwndSourceHook = new HwndSourceHook(WndProc);
-                source.AddHook(hwndSourceHook);
+                if(refreshMousePositionTimer.Enabled)
+                {
+                    refreshMousePositionTimer.Stop();
+                }
+
+                refreshMousePositionTimer.Dispose();
             }
+
+            refreshMousePositionTimer = new Timer
+            {
+                AutoReset = true,
+                Interval = 1000 / 10
+            };
+            refreshMousePositionTimer.Elapsed += RefreshMousePositionTimer_Elapsed;
+            refreshMousePositionTimer.Start();
         }
 
-        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            if (IgnoreInput)
-            {
-                //
-                if (msg == (int)WindowStyles.WM_NCHITTEST)
-                    return (IntPtr)WindowStyles.HTTRANSPARENT;
-                else
-                    return IntPtr.Zero;
-            }
+        static int marginHorziontal = 100;
+        static int marginVertical = 100;
 
-            return IntPtr.Zero;
+        private void RefreshMousePositionTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var point = GetMousePosition();
+            bool isNearHorizontal = false;
+            bool isNearVertical = false;
+
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                if (point.X < (this.Left + this.Width + marginHorziontal))
+                    isNearHorizontal = true;
+                else
+                    isNearHorizontal = false;
+
+                if (point.Y < (this.Top + this.Height + marginVertical))
+                    isNearVertical = true;
+                else
+                    isNearVertical = false;
+
+                if (isNearHorizontal && isNearVertical)
+                    this.Opacity = 0;
+                else
+                    this.Opacity = 1;
+            });
+        }
+
+        void DisableIgnoreInput()
+        {
+            if (refreshMousePositionTimer != null)
+            {
+                if (refreshMousePositionTimer.Enabled)
+                {
+                    refreshMousePositionTimer.Stop();
+                }
+
+                refreshMousePositionTimer.Dispose();
+            }
         }
 
         private void Shell_Unloaded(object sender, RoutedEventArgs e)
         {
-            if (IsWindowProcHooked)
-            {
-                source.RemoveHook(hwndSourceHook);
-            }
+
         }
 
         enum WindowStyles
